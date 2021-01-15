ee100f64ee8cf1ef89ba70b78bba5e7b8c7b5b74
GiovanniR-S <giovannirobertssilva@outlook.com>
2021-01-14 21:24:08 -0300

    macânica do jogo de memória terminada

--- js/js.js
+++ js/js.js
@@ -1,10 +1,65 @@
 const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let fistCard, secondCard;
+let lockBoard = false;
 
 function flipCard(){
-    this.classList.toggle('flip');
+    if(lockBoard) return;
+    if(this === fistCard) return;
+
+    this.classList.add('flip');
+    if(!hasFlippedCard){
+        hasFlippedCard = true;
+        fistCard = this;
+        return;
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMath();
+
 }
 
+function checkForMath() {
+    if(fistCard.dataset.card === secondCard.dataset.card){
+        disableCards();
+        return;
+    }
+
+    unFlipCard();
+}
+
+function disableCards() {
+    fistCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+function unFlipCard(){
+    lockBoard = true;
+    setTimeout(() => {
+        fistCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+
+    }, 1500);
+}
+
+function resetBoard(){
+    [hasFlippedCard, lockBoard] = [false, false];
+    [fistCard, secondCard] = [null, null];
+}
+
+(function shuffle(){
+    cards.forEach((card) => {
+        let randomPosition = Math.floor(Math.random() * 12);
+        card.style.order = randomPosition;
+    })
+})();
+
 cards.forEach((card) => {
-    card.addEventListener('click', flipCard)
+    card.addEventListener('click', flipCard);
 })
 
